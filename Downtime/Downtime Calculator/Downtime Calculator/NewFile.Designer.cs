efdee74201d42a26f8bb671609a4e19caaca7439
TelinThoth <Telin.thoth@gmail.com>
2017-12-04 14:00:44 -0500

    Add files via upload

---
+++ Downtime/Downtime Calculator/Downtime Calculator/NewFile.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Downtime_Calculator
+{
+    partial class NewFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(86, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Campaign name:";
+            // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(104, 6);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(100, 20);
+            this.txtbxName.TabIndex = 1;
+            this.txtbxName.TextChanged += new System.EventHandler(this.txtbxName_TextChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(15, 60);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(129, 60);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 3;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // NewFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 99);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtbxName);
+            this.Controls.Add(this.lblName);
+            this.Name = "NewFile";
+            this.Text = "New Campaign";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtbxName;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAccept;
+    }
+}
---
+++ Downtime/Downtime Calculator/Downtime Calculator/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Downtime_Calculator
+{
+    class Player
+    {
+        [Flags]
+        enum flags
+        {
+            none = 0x0,
+            raise = 0x1,
+            resurection = 0x2,
+            trueRes = 0x4,
+            activeRF = 0x8,
+            monthly = 0x16,
+            failsClears = 0x32,
+            depToRF = 0x64,
+            activeAcc = 0x128
+        }
+
+        string name;
+
+        UInt32 bankAccount;
+        UInt32 RFAccount;
+
+
+    }
+}
